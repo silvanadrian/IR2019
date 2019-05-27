3227ce67ca414b46ecfb23b95d6f486a557d65ee
Silvan Adrian <hallo@silvanadrian.ch>
2019-05-27 18:42:55 +0200

    newest changes

--- assignments/assignment_2/trec_topic_to_title.py
+++ assignments/assignment_2/trec_topic_to_title.py
@@ -9,25 +9,29 @@ if len(sys.argv) < 2 or not os.path.isfile(sys.argv[1]):
 
 cleanTextRegex = re.compile('[^a-zA-Z]')
 count = 0
-outFilepath = 'data/'+os.path.basename(sys.argv[1])
+outFilepath = 'data/'+"queries.txt"
+line_num = 0
 
 with open(outFilepath, 'w') as outputFile:
     with open(sys.argv[1], 'r') as inputFile:
         currentId = ''
-        for inLine in inputFile.readlines():
+        lines = inputFile.readlines()
+        for inLine in lines:
+            line_num += 1
             if inLine.startswith('<num> Number:'):
                 currentId = inLine.replace('<num> Number:', '').strip()
             if inLine.startswith('<title>'):
                 text = inLine.replace('<title>', '').strip()
-                # clean text
                 text = cleanTextRegex.sub(' ', text).lower()
-                # remove multiple whitespaces
                 text = text.replace('    ',' ').replace('   ',' ').replace('  ',' ')
-
+                if not text:
+                	text = lines[line_num]
+                	text = cleanTextRegex.sub(' ', text).lower()
+                	text = text.replace('    ',' ').replace('   ',' ').replace('  ',' ')
                 wordList = []
                 for w in text.split(' '):
                     if w:
-                            cleaned = w.strip()
+                        cleaned = w.strip()
                         wordList.append(cleaned)
                 outputText = ' '.join(wordList)
 
